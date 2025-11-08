45b179b5bcdd6ded8573685839ea6337fb3f9d5c
rodrigo1163 <rodrigo.sena1163@gmail.com>
2025-11-08 13:17:03 -0400

    feat: create database with authentication

--- apps/server/env.ts
+++ apps/server/env.ts
@@ -2,6 +2,8 @@ import z from "zod";
 
 const envSchema = z.object({
 	PORT: z.coerce.number().default(3333),
+	BETTER_AUTH_SECRET: z.string(),
+	DATABASE_URL: z.url(),
 });
 
 export const env = envSchema.parse(process.env);
